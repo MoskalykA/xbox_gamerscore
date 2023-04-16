1b1af90452800c63828f7eb9c1f3a9e7959049b3
MoskalykA <100430077+MoskalykA@users.noreply.github.com>
2023-04-16 19:08:44 +0200

    first commit

---
+++ out/index.js
@@ -0,0 +1,34 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const superagent_1 = __importDefault(require("superagent"));
+const jsdom_1 = __importDefault(require("jsdom"));
+const { JSDOM } = jsdom_1.default;
+const search = (gamerTag) => {
+    const promise = new Promise((resolve, reject) => {
+        superagent_1.default
+            .get(`https://www.xboxgamertag.com/search/${gamerTag}`)
+            .end((err, res) => {
+            var _a, _b, _c;
+            if (err)
+                reject("An error with the request");
+            const dom = new JSDOM(res.text);
+            for (const span of dom.window.document.querySelectorAll("span")) {
+                if ((_a = span === null || span === void 0 ? void 0 : span.textContent) === null || _a === void 0 ? void 0 : _a.includes("Gamerscore")) {
+                    resolve(Number((_c = (_b = span.parentNode) === null || _b === void 0 ? void 0 : _b.textContent) === null || _c === void 0 ? void 0 : _c.replace("Gamerscore", "").replace(/[\r\n]+/gm, "").replace(/\s/g, "")));
+                }
+            }
+            reject("The gameScore was not found");
+        });
+    });
+    return promise;
+};
+search("hello")
+    .then((gameScore) => {
+    console.log(gameScore);
+})
+    .catch((reason) => {
+    console.error(reason);
+});
---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import superagent from "superagent";
+import jsdom from "jsdom";
+
+const { JSDOM } = jsdom;
+const search = (gamerTag: string): Promise<number> => {
+  const promise: Promise<number> = new Promise((resolve, reject) => {
+    superagent
+      .get(`https://www.xboxgamertag.com/search/${gamerTag}`)
+      .end((err, res) => {
+        if (err) reject("An error with the request");
+
+        const dom = new JSDOM(res.text);
+        for (const span of dom.window.document.querySelectorAll("span")) {
+          if (span?.textContent?.includes("Gamerscore")) {
+            resolve(
+              Number(
+                span.parentNode?.textContent
+                  ?.replace("Gamerscore", "")
+                  .replace(/[\r\n]+/gm, "")
+                  .replace(/\s/g, "")
+              )
+            );
+          }
+        }
+
+        reject("The gameScore was not found");
+      });
+  });
+
+  return promise;
+};
+
+search("hello")
+  .then((gameScore: number) => {
+    console.log(gameScore);
+  })
+  .catch((reason: string) => {
+    console.error(reason);
+  });
